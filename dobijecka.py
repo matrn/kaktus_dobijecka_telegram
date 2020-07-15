88a8f4bbe5e3c3f0284cd506e98bf0a1ca845bbc
matrn <trnka.matej@seznam.cz>
2020-07-15 16:43:12 +0200

    kaktus dobijecka parser fixed

--- dobijecka.py
+++ dobijecka.py
@@ -20,12 +20,15 @@ class Dobijecka:
 
 
 	def parse_data_html(self, html):
-		for node in HTMLParser(html).css('div.wrapper > h2.uppercase + h3.uppercase.text-drawn'):
-			node_data = node.text().strip().lower()
-			if 'pokud si dneska' in node_data and 'od' in node_data and 'do' in node_data:
-				return node_data
-
-		return None
+		#for node in HTMLParser(html).css('div.wrapper > h2.uppercase + h3.uppercase.text-drawn'):
+		for node in HTMLParser(html).css('.box-bubble-inner .journal-content-article p'):
+			#print(node.text())
+			raw_node_data = node.text().replace('\xa0', ' ').replace('  ', ' ')
+			node_data = raw_node_data.strip().lower()
+			if ' dnes' in node_data and 'dobij' and (' od ' in node_data or ' mezi ' in node_data) and (' do ' in node_data or ' a ' in node_data):
+				return raw_node_data, node_data   #raw data and stripped + lower data
+		
+		return None, None
 		
 		
 	def parse_date(self, data):
@@ -33,7 +36,7 @@ class Dobijecka:
 			if i < max_i: return list[i]
 			else: return None
 
-		data = data.replace('  ', ' ').replace('. ', '.').replace(': ', ':')
+		data = data.replace('  ', ' ').replace(': ', ':')   #replace('. ', '.')
 		list = data.split(' ')
 		max_i = len(list)
 
@@ -41,14 +44,17 @@ class Dobijecka:
 		start = None
 		stop = None
 		for i in range(max_i):
-			if 'dneska' == list[i]:
-				date = try_next(list, i+1, max_i)
+			if 'dnes' in list[i]:
+				date = try_next(list, i+1, max_i) + try_next(list, i+2, max_i)
+				#date = ''.join(i for i in date if i.isdigit() or i == '.')   #keep only numbers and dots
 
 			elif 'od' == list[i]:
-							start = try_next(list, i+1, max_i)
+				start = try_next(list, i+1, max_i)
+				if ':' not in start: start = str(start) + ':00'
 
 			elif 'do' == list[i]:
-							stop = try_next(list, i+1, max_i)
+				stop = try_next(list, i+1, max_i)
+				if ':' not in stop: stop = str(stop) + ':00'
 
 
 		#print(data)
@@ -58,7 +64,13 @@ class Dobijecka:
 
 	def is_date_today(self, date):
 		try:
-			if datetime.strptime(date, '%d.%m.%Y').date() == datetime.today().date():
+			#parse date from string
+			if date.endswith('.'):
+				date_date = datetime.strptime(date, '%d.%m.').date()
+			else:
+				date_date = datetime.strptime(date, '%d.%m.%Y').date()
+			
+			if date_date == datetime.today().date():
 				return True
 			else:
 				return False
@@ -70,13 +82,13 @@ class Dobijecka:
 		html = self.get_data()
 		if not html: return None, { 'error':'GET request failed' }
 
-		data = self.parse_data_html(html)
+		raw_data, data = self.parse_data_html(html)
 		if not data: return None, { 'error':'HTML parse failed', 'raw':html }
 
 		date, start_time, stop_time = self.parse_date(data)
 		if not date: return None, { 'error':'DATE parsing failed', 'raw':data }
 		
-		return self.is_date_today(date), { 'date':date, 'start':start_time, 'stop':stop_time, 'msg':data}
+		return self.is_date_today(date), { 'date':date, 'start':start_time, 'stop':stop_time, 'msg':raw_data}
 
 
 
